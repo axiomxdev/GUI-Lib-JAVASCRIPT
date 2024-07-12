9aab5df9106f2ab73514fd5b6852b44d5dc3a764
axiom <107348304+axiomxdev@users.noreply.github.com>
2024-07-12 13:16:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -205,25 +205,3 @@ class MyPopup {
         return this; // Permet de chaîner les méthodes
     }
 }
-
-window.addEventListener('load', function () {
-    // Exemple de construction de la popup.
-    new MyPopup({
-        'name': 'GUI Lib JAVASCRIPT',
-        'main-color': '#45414F',
-        'second-color': '#302D39',
-        'size-width': '400px',
-        'size-height': '300px',
-        'positionX': '400px',
-        'positionY': '100px',
-        'font-color': '#ffffff',
-        'close-button-color': '#ffffff',
-        'separation': '10px'
-    }).addButton({
-        'label': 'Button',
-        'size-height': '50px',
-        'font-color': '#ffffff',
-        'Xseparation': '10px',
-        'font-size': '18px' 
-    });
-});
